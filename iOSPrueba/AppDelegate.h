26dacf11ae53cf8306ab8dababb2a9b4e37da889
Manuel Alberto Toro Morales <manuel@MacBook-Pro-de-Manuel.local>
2013-12-03 11:18:49 -0300

    Primera versión subida

---
+++ iOSPrueba/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSPrueba
+//
+//  Created by Manuel Alberto Toro Morales on 03-12-13.
+//  Copyright (c) 2013 InnovaGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSPrueba/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSPrueba
+//
+//  Created by Manuel Alberto Toro Morales on 03-12-13.
+//  Copyright (c) 2013 InnovaGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
